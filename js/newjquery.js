3464d38ecf96393d99b0b3f0919a51adaf824b8c
NLBSWebDev <webdesign@nlbstudios.com>
2017-06-02 16:54:12 -0400

    first commit

---
+++ js/newjquery.js
@@ -0,0 +1,16 @@
+$('#about1').hide();
+$('#about2').hide();
+$('#about3').hide();
+
+
+$(document).ready(function() {
+    $('#about1').fadeIn(2000);
+});
+
+$(document).ready(function(){
+    $('#about2').fadeIn(2000);
+});
+
+$(document).ready(function(){
+    $('#about3').fadeIn(2000);
+});
