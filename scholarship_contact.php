8fd6f8adb95f0a8340c6e200d3fe3c15f5c6cb6a
NLBSWebDev <webdesign@nlbstudios.com>
2017-07-28 15:37:58 -0400

    last commit

---
+++ scholarship_contact.php
@@ -0,0 +1,36 @@
+
+
+<?php
+$field_name = $_POST['cf_name'];
+$field_phone = $_POST['cf_phone'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'admin@restart-recovery.org';
+$subject = 'Scholarship Request';
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'Phone: '.$field_phone."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to crisis@restart-recovery.org');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
---
+++ testimonial.php
@@ -0,0 +1,32 @@
+
+
+<?php
+$field_name = $_POST['cf_name'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'admin@restart-recovery.org';
+$subject = 'Testimonial Submission';
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to crisis@restart-recovery.org');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
